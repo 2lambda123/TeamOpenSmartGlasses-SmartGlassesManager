9cae49065453f5f93b38c434d7ddb4dd3fe670bf
Restyled.io <commits@restyled.io>
2024-01-29 15:46:12 +0000

    Restyled by clang-format

--- SGM_android_library/SGMLib/src/main/java/com/teamopensmartglasses/sgmlib/TPABroadcastReceiver.java
+++ SGM_android_library/SGMLib/src/main/java/com/teamopensmartglasses/sgmlib/TPABroadcastReceiver.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
-
 import com.teamopensmartglasses.sgmlib.events.CommandTriggeredEvent;
 import com.teamopensmartglasses.sgmlib.events.FocusChangedEvent;
 import com.teamopensmartglasses.sgmlib.events.FocusRequestEvent;
@@ -19,54 +18,51 @@ import com.teamopensmartglasses.sgmlib.events.KillTpaEvent;
 import com.teamopensmartglasses.sgmlib.events.SmartRingButtonOutputEvent;
 import com.teamopensmartglasses.sgmlib.events.SpeechRecFinalOutputEvent;
 import com.teamopensmartglasses.sgmlib.events.SpeechRecIntermediateOutputEvent;
-
+import java.io.Serializable;
 import org.greenrobot.eventbus.EventBus;
 
-import java.io.Serializable;
-
 public class TPABroadcastReceiver extends BroadcastReceiver {
-    private String filterPkg;
-    private Context context;
-    public String TAG = "SGMLib_TPABroadcastReceiver";
+  private String filterPkg;
+  private Context context;
+  public String TAG = "SGMLib_TPABroadcastReceiver";
 
-    public TPABroadcastReceiver(Context myContext) {
-        this.context = myContext;
-        this.filterPkg = SGMGlobalConstants.TO_TPA_FILTER;
-        IntentFilter intentFilter = new IntentFilter(this.filterPkg);
-        this.context.registerReceiver(this, intentFilter, RECEIVER_EXPORTED);
+  public TPABroadcastReceiver(Context myContext) {
+    this.context = myContext;
+    this.filterPkg = SGMGlobalConstants.TO_TPA_FILTER;
+    IntentFilter intentFilter = new IntentFilter(this.filterPkg);
+    this.context.registerReceiver(this, intentFilter, RECEIVER_EXPORTED);
+  }
+
+  public void onReceive(Context context, Intent intent) {
+    String eventId = intent.getStringExtra(EVENT_ID);
+    Serializable serializedEvent = intent.getSerializableExtra(EVENT_BUNDLE);
+
+    // map from id to event
+    switch (eventId) {
+    case CommandTriggeredEvent.eventId:
+      EventBus.getDefault().post((CommandTriggeredEvent)serializedEvent);
+      break;
+    case KillTpaEvent.eventId:
+      EventBus.getDefault().post((KillTpaEvent)serializedEvent);
+      break;
+    case SpeechRecIntermediateOutputEvent.eventId:
+      EventBus.getDefault().post(
+          (SpeechRecIntermediateOutputEvent)serializedEvent);
+      break;
+    case SpeechRecFinalOutputEvent.eventId:
+      EventBus.getDefault().post((SpeechRecFinalOutputEvent)serializedEvent);
+      break;
+    case SmartRingButtonOutputEvent.eventId:
+      EventBus.getDefault().post((SmartRingButtonOutputEvent)serializedEvent);
+      break;
+    case GlassesTapOutputEvent.eventId:
+      EventBus.getDefault().post((GlassesTapOutputEvent)serializedEvent);
+      break;
+    case FocusChangedEvent.eventId:
+      EventBus.getDefault().post((FocusChangedEvent)serializedEvent);
+      break;
     }
+  }
 
-    public void onReceive(Context context, Intent intent) {
-        String eventId = intent.getStringExtra(EVENT_ID);
-        Serializable serializedEvent = intent.getSerializableExtra(EVENT_BUNDLE);
-
-        //map from id to event
-        switch (eventId) {
-            case CommandTriggeredEvent.eventId:
-                EventBus.getDefault().post((CommandTriggeredEvent) serializedEvent);
-                break;
-            case KillTpaEvent.eventId:
-                EventBus.getDefault().post((KillTpaEvent) serializedEvent);
-                break;
-            case SpeechRecIntermediateOutputEvent.eventId:
-                EventBus.getDefault().post((SpeechRecIntermediateOutputEvent) serializedEvent);
-                break;
-            case SpeechRecFinalOutputEvent.eventId:
-                EventBus.getDefault().post((SpeechRecFinalOutputEvent) serializedEvent);
-                break;
-            case SmartRingButtonOutputEvent.eventId:
-                EventBus.getDefault().post((SmartRingButtonOutputEvent) serializedEvent);
-                break;
-            case GlassesTapOutputEvent.eventId:
-                EventBus.getDefault().post((GlassesTapOutputEvent) serializedEvent);
-                break;
-            case FocusChangedEvent.eventId:
-                EventBus.getDefault().post((FocusChangedEvent) serializedEvent);
-                break;
-        }
-    }
-
-    public void destroy(){
-        this.context.unregisterReceiver(this);
-    }
+  public void destroy() { this.context.unregisterReceiver(this); }
 }
